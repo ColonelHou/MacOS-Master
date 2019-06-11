cc360ee981cddd69ee902dcb9fd3176e4ec31ed1
DuerOS <houningning@MacOS.local>
2019-06-11 19:21:21 +0800

    SpringBoot yaml

--- macos-springboot/src/main/java/org/macos/java/springboot/HelloController.java
+++ macos-springboot/src/main/java/org/macos/java/springboot/HelloController.java
@@ -1,6 +1,8 @@
 package org.macos.java.springboot;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -9,8 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @RequestMapping("/")
-    public String hello() {
-        return "Hello + World ";
+    @RequestMapping(value = "/say", method = RequestMethod.GET)
+    public String hello(@RequestParam String title) {
+        System.out.println("say ------> hello world" + title);
+        return "Hello + World " + title;
+    }
+
+    @RequestMapping(name = "/haha")
+    public long say(@RequestParam String abc) {
+        System.out.println("say =======> 1000" + abc);
+        return 1000;
     }
 }
