4910c1208045ff2c4cac63e7163e144eaefc32cb
houningning <houningning@yiche.com>
2019-07-04 18:07:19 +0800

    eureka-server

--- m.sh
+++ m.sh
@@ -1,5 +1,6 @@
 rm -rf /Users/houningning/Documents/mywksp/git/MacOS-Master/macos-*   
 cp -r /Users/houningning/Documents/mywksp/MacOS-Master/macos-* /Users/houningning/Documents/mywksp/git/MacOS-Master/
+cp -r /Users/houningning/Documents/mywksp/MacOS-Master/eureka-server /Users/houningning/Documents/mywksp/git/MacOS-Master
 
 git add .
 git commit -m $1
